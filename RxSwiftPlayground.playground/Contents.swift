f58615d50ff96888b6221800471eea2b127fed25
Lucas <lucaspedrazoli@gmail.com>
2020-11-04 14:31:04 -0300

    deferred and single example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -86,3 +86,61 @@ example(of: "Create") {
               onDisposed: { print("Disposed") })
     .disposed(by: disposeBag)
 }
+
+example(of: "Deferred") {
+  let disposeBag = DisposeBag()
+  var flip = false
+  let factory: Observable<Int> = Observable.deferred {
+    flip.toggle()
+    if flip {
+      return Observable.of(1, 2, 3)
+    } else {
+      return Observable.of(4, 5, 6)
+    }
+  }
+
+  for _ in 0...3 { factory.subscribe(onNext: {
+    print($0, terminator: "")
+  }).disposed(by: disposeBag)
+    print()
+  }
+}
+
+example(of: "Single") {
+  let disposeBag = DisposeBag()
+  enum FileReadError: Error {
+    case fileNotFound, unreadable, encodingFailed
+  }
+  func loadText(from name: String) -> Single<String> {
+    return Single.create { single in
+      let disposable = Disposables.create()
+      guard let path = Bundle.main.path(forResource: name, ofType: "txt") else {
+        single(.error(FileReadError.fileNotFound))
+        return disposable
+      }
+
+      guard let data = FileManager.default.contents(atPath: path) else {
+        single(.error(FileReadError.unreadable))
+        return disposable
+      }
+
+      guard let contents = String(data: data, encoding: .utf8) else {
+        single(.error(FileReadError.encodingFailed))
+        return disposable
+      }
+
+      single(.success(contents))
+      return disposable
+    }
+  }
+
+  loadText(from: "Copyright")
+    .subscribe {
+      switch $0 {
+      case .success(let string):
+        print(string)
+      case .error(let error):
+        print(error)
+      }
+  }
+}
